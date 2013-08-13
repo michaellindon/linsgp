9500f096d85bbbcd2e50fdb115afa68655878e38
Michael Lindon <michael.s.lindon@gmail.com>
2013-08-12 23:46:48 -0400

    Progress on building the correlation matrices

--- main.cpp
+++ main.cpp
@@ -15,17 +15,52 @@
 
 class particles{
 	public:
-		std::vector<double> phi,phit;
+		std::vector<double> phi,phit,K,Kt,Z;
+
 };
+
+
+//Constructs Correlation matrix K using the first "n" rows of X i.e. K(X1:n)
+void corr(particles &particle,std::vector<double> &X, int n, int Dim){
+	int i,j,d;
+
+	particle.K.resize(n*n,0);
+	particle.Kt.resize(n*n,0);
+	for (i = 0; i < n; ++i)
+	{
+		for (j = 0; j <= i; ++j)
+		{
+			for (d = 0; d < Dim; ++d)
+			{
+				particle.K[i*n+j]+=particle.phi[d]*(X[i*n + d] - X[j*n + d])*(X[i*n + d] - X[j*n + d]);
+				particle.Kt[i*n+j]+=particle.phit[d]*(X[i*n + d] - X[j*n + d])*(X[i*n + d] - X[j*n + d]);
+			}
+			//particle.K[Dim]+=particle.phi[Dim]*(particle.Z[i]-particle.Z[j])*(particle.Z[i] - particle.Z[j]);
+
+			particle.K[i*n+j]=exp(-particle.K[i*n+j]);
+			particle.K[j*n+i]=particle.K[i*n+j];
+
+			particle.Kt[i*n+j]=exp(-particle.Kt[i*n+j]);
+			particle.Kt[j*n+i]=particle.Kt[i*n+j];
+
+		}
+	}
+}
+
 
 using namespace std;
 
-extern "C" void linsgp(int *np, int *niter, double *mphi, double *mphit, double *vphi, double *vphit)
+extern "C" void linsgp(double *XR, int *X_nrow, int *X_ncol, int *np, int *niter, double *mphi, double *mphit, double *vphi, double *vphit)
 {
 	int i,d;
-	int p=10; //p ~ number of dimensions
+	int nobs=*X_nrow; //nobs ~ number of observations
+	int Dim=*X_ncol; //Dim ~ dimension of Domain, excluding latent variable.
 	int nthreads=1; //nthreads ~ number of threads. 1 by default.
 	int rank=0; //thread identifier. 0 by default.
+
+	//Convert C-style array of doubles to C++-style vector of doubles
+	std::vector<double> X(*X_ncol * *X_nrow);
+	std::copy (XR, XR+(*X_nrow * *X_ncol), X.begin() );
 
 	particles * particle; 
 	trng::normal_dist<> normal(0.0,1.0);
@@ -39,13 +74,13 @@ extern "C" void linsgp(int *np, int *niter, double *mphi, double *mphit, double 
 			<< std::endl;
 		std::cout << "Please consider using fewer particles." 
 			<< std::endl;
-	//	return EXIT_FAILURE;
+		//	return EXIT_FAILURE;
 	}
 
 
 	//Query maximum threads supported by hardware
 	nthreads=omp_get_max_threads();
-	
+
 	//Set number of threads to the supported maximum
 #ifdef _OPENMP
 	omp_set_num_threads(nthreads);
@@ -69,30 +104,37 @@ extern "C" void linsgp(int *np, int *niter, double *mphi, double *mphit, double 
 
 
 	//Initialize particles with draws from the posterior
-trng::lognormal_dist<> lognormal_phi(*mphi,*vphi);
-trng::lognormal_dist<> lognormal_phit(*mphit,*vphit);
-#pragma omp parallel for private(rank,i,d)
+	trng::lognormal_dist<> lognormal_phi(*mphi,*vphi);
+	trng::lognormal_dist<> lognormal_phit(*mphit,*vphit);
+#pragma omp parallel for private(rank,i,d) 
 	for (i = 0; i < *np; ++i)
 	{
 		rank=omp_get_thread_num();
-		particle[i].phi.resize(p+1);
-		particle[i].phit.resize(p);
-	
-		for (d = 0; d < p; ++d)
-		{
-		particle[i].phi[d] = lognormal_phi(stream[rank]);
-		particle[i].phit[d] = lognormal_phi(stream[rank]);
-		}
-		particle[i].phi[p] = lognormal_phi(stream[rank]);
+		particle[i].phi.resize(Dim+1,0);
+		particle[i].phit.resize(Dim,0);
+		particle[i].K.reserve(nobs * nobs);
+		particle[i].Kt.reserve(nobs * nobs);
 
+		for (d = 0; d < Dim; ++d)
+		{
+			particle[i].phi[d] = lognormal_phi(stream[rank]);
+			particle[i].phit[d] = lognormal_phi(stream[rank]);
+		}
+		particle[i].phi[Dim] = lognormal_phi(stream[rank]);
+
+		corr(particle[i],X,5,Dim);
 	}
 
 
-	cout << particle[1].phi[1] << endl;
-	cout << particle[1].phi[2] << endl;
-	cout << particle[2].phi[1] << endl;
+	for (int i = 0; (unsigned int) i < particle[1].K.size(); ++i)
+	{
+		cout << particle[1].K[i] << endl;
+	}
+
 	delete[] particle; //Free memory
 	delete[] stream;
 
-//	return 0;
+	//	return 0;
 }
+
+
